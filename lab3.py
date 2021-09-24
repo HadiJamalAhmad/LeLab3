b1ea8a008e9c7bb447f7160d045a85068742107f
LENOVO <hadi.jamalahmad@gmail.com>
2021-09-24 15:59:32 +0200

    Final Commit

--- lab3.py
+++ lab3.py
@@ -52,7 +52,7 @@ def data_hours(data):
             st = time.perf_counter()
             r = fn(*a, **kw)
             print(f"{fn.__name__} execution: {time.perf_counter() - st} seconds")
-            print(f"function data_hours : {fn.__name__} execution: {time.perf_counter() - st} seconds", file=open("tplab3.txt", "a"))
+        #   print(f"function data_hours : {fn.__name__} execution: {time.perf_counter() - st} seconds", file=open("tplab3.txt", "a"))
             return r
         
         return _timing
